2c8087c82a016b27d3b41bda9e0835645eb63cd5
unknown <gonzalogomez922@gmail.com>
2023-03-22 14:00:53 -0300

    creando data y archivos base

---
+++ src/js/data/calzado.js
@@ -0,0 +1,13 @@
+export const calzadoData = [
+    {
+        marca: "aca va la marca",
+        sexo: "h m o",
+        modelo: "",
+        tipo: "urbano depor",
+        talle: "",
+        precio: 0,
+        stock: true,
+        url: "aca va la url de la img",
+    }, 
+]
+
